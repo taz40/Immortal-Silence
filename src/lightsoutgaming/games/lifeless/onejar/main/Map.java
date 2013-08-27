85cb0a57c6c966ef8d32a5fff11e0ba04ebb80df
taz40 <ldspcjunkie@gmail.com>
2013-08-27 16:47:38 -0600

    8/27/13 4:46 pm

--- src/lightsoutgaming/games/lifeless/onejar/main/Map.java
+++ src/lightsoutgaming/games/lifeless/onejar/main/Map.java
@@ -1,7 +1,41 @@
 package lightsoutgaming.games.lifeless.onejar.main;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Map {
 
+	int zombiesatonetime;
+	double zobiechance;
+	int width, height;
+	int background;
 	
+	public Map(String path){
+		File file = new File(path);
+		FileReader fr = null;
+		try {
+			fr = new FileReader(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		BufferedReader br = new BufferedReader(fr);
+		
+		try {
+			zombiesatonetime = Integer.parseInt(br.readLine());
+			zobiechance = Double.parseDouble(br.readLine());
+			width = Integer.parseInt(br.readLine());
+			height = Integer.parseInt(br.readLine());
+		} catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 }
